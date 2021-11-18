dc43cfa66decfcaba223ff49b361309f50d9b245
gesora96 <nahidahmed9933@gmail.com>
2021-11-18 23:34:26 +0600

    some route created without testing

--- index.js
+++ index.js
@@ -23,12 +23,47 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const database = client.db(`${process.env.DB_NAME}`);
-    const topPlaceCollection = database.collection('places');
-    console.log('database connect successfully');
-  } catch {
+    const topPlaceCollection = database.collection('all_places');
+    const orderCollection = database.collection('orders');
+
+    // GET all top places
+
+    app.get('/top-places', async (req, res) => {
+      const cursor = topPlaceCollection.find({});
+      const places = await cursor.toArray();
+      res.send(places);
+    });
+
+    // GET a single place by ID
+
+    app.get('/place/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const place = await topPlaceCollection.findOne(query);
+      res.send(place);
+    });
+
+    // GET all orders
+
+    app.get('all-orders', async (req, res) => {
+      const cursor = orderCollection.find({});
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
+
+    // DELETE a single order by ID
+
+    app.delete('/order/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.json(result);
+    });
+  } finally {
     // await client.close()
   }
 }
+
 run().catch(console.dir);
 
 app.get('/', (req, res) => res.send('Welcome to Dreamy Travel Server'));
